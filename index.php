f5b6c9c2d7076385345ad4459951bacf3881a5ca
Adam Prasetia <adam.prasetia@gmail.com>
2015-10-16 23:38:56 +0700

    .

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
 <html lang="en">
 	<head>
 		<title>Latihan Git</title>
+		<link href="assets/css/style.css">
 	</head>
 	<body>
-		<h1>Latihan Git</h1>	
+		<div class="w1100">
+			<h1>Memulai Git - Dasar Git</h1>	
+			<p>Jadi, sebenarnya apa yang dimaksud dengan Git? Ini adalah bagian penting untuk dipahami, karena jika anda memahami apa itu Git dan cara kerjanya, maka dapat dipastikan anda dapat menggunakan Git secara efektif dengan mudah. Selama mempelajari Git, cobalah untuk melupakan VCS lain yang mungkin telah anda kenal sebelumnya, misalnya Subversion dan Perforce. Git sangat berbeda dengan sistem-sistem tersebut dalam hal menyimpan dan memperlakukan informasi yang digunakan, walaupun antar-muka penggunanya hampir mirip. Dengan memahami perbedaan tersebut diharapkan dapat membantu anda menghindari kebingungan saat menggunakan Git.</p>
+			</div>
 	</body>
 </html>
